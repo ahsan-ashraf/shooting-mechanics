20b8a25e8bb16c72df70d5ae9c0cebc71d57d6d3
ahsanozi <ahsanozi68@gmail.com>
2020-05-31 16:18:57 +0500

    Grenade Mechanics added

---
+++ Assets/1. Weapon Mechanics/Grenades/Scripts/Grenade.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class Grenade : MonoBehaviour {
+
+    public float Delay = 3.0f;
+    public float ExplosionRadius = 20.0f;
+    public float ExplosionForce = 10000.0f;
+    public GameObject ExplosionEffect;
+    
+    private float CountDown = 0.0f;
+    private bool Exploded = false;
+
+    // Start is called before the first frame update
+    void Start() {
+        CountDown = Delay;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        CountDown -= Time.deltaTime;
+        if (CountDown <= 0 && !Exploded) {
+            Explode();
+            Exploded = true;
+        }
+    }
+    private void Explode() {
+        GameObject explosionEffect = Instantiate(ExplosionEffect, transform.position, transform.rotation);
+        Destroy(explosionEffect, 1.5f);
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, ExplosionRadius);
+        foreach (Collider collider in colliders) {
+            collider.SendMessage("Destroy", SendMessageOptions.DontRequireReceiver);
+        }
+
+        colliders = Physics.OverlapSphere(transform.position, ExplosionRadius);
+        foreach (Collider collider in colliders) {
+            Rigidbody rb = collider.GetComponent<Rigidbody>();
+            if (rb != null) {
+                rb.AddExplosionForce(ExplosionForce, transform.position, ExplosionRadius);
+            }
+            collider.SendMessage("Destroy", SendMessageOptions.DontRequireReceiver);
+        }
+        Destroy(gameObject);
+    }
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, ExplosionRadius);
+    }
+}
---
+++ Assets/1. Weapon Mechanics/Grenades/Scripts/GrenadeThrower.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class GrenadeThrower : MonoBehaviour {
+
+    public float ThrowForce = 25.0f;
+    public GameObject GrenadePrefab;
+
+    void Update() {
+        if (Input.GetButtonDown("Fire1")) {
+            ThrowGrenade();
+        }
+    }
+    private void ThrowGrenade() {
+        GameObject grenade = Instantiate(GrenadePrefab, transform.position, transform.rotation);
+        Rigidbody rb = grenade.GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * ThrowForce, ForceMode.VelocityChange);
+    }
+}
